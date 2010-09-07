10bd6967f71e3d66353ddfbaae366a131ebaac99
karevn <karev.n@gmail.com>
2010-09-07 16:53:59 +0600

    Added an option model_class_name.

--- lib/jrails_auto_complete.rb
+++ lib/jrails_auto_complete.rb
@@ -6,13 +6,14 @@ module JrailsAutoComplete
   module ClassMethods
     def auto_complete_for(object_name, method_name, options = {})
       self.send(:define_method, "auto_complete_for_#{object_name}_#{method_name}") do
+        model_class_name = options.delete(:model_class_name) || object_name
         find_options = {
           :conditions => [ "LOWER(#{method_name}) LIKE ?", '%' + params[object_name][method_name].downcase + '%' ],
           :order => "#{method_name} ASC",
           :limit => 10
         }.merge!(options)
 
-        @items = object_name.to_s.camelize.constantize.find(:all, find_options)
+        @items = model_class_name.to_s.camelize.constantize.find(:all, find_options)
         render :inline => "<%= auto_complete_result @items, '#{method_name}' %>"
       end
     end
