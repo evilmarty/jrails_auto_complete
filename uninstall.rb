4821bcf1f36d688e65d54070aca025231fd64cbf
karevn <karev.n@gmail.com>
2010-09-07 16:52:52 +0600

    Fixed uninstallation crash.

--- uninstall.rb
+++ uninstall.rb
@@ -2,6 +2,6 @@ puts "Removing files..."
 dir = "javascripts"
 ["jrails.autocomplete.js"].each do |js_file|
 	dest_file = File.join(RAILS_ROOT, "public", dir, js_file)
-	FileUtils.rm_r(src_file, dest_file)
+	FileUtils.rm_r(dest_file)
 end
 puts "Files removed - Uninstallation complete!"
